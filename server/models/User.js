c9b6aa55f2fb28d5d2910a613c5b7145d4369f41
Lewis Benson <tm.lewisbenson@gmail.com>
2022-12-23 11:05:15 -0500

    add ref to characters on user

--- server/models/User.js
+++ server/models/User.js
@@ -6,16 +6,16 @@ const { Schema } = mongoose;
 
 
 const UserSchema = new Schema({
-  user_id: { type: String, required: true },
   username: {
     type: String,
     required: true,
     unique: true,
   },
+  password: { type: String, required: true },
   role: { type: String, required: true },
-
+  characters: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Characters' }],
 });
 
 
 
-module.exports = mongoose.model('user', UserSchema);
+module.exports = mongoose.model('Users', UserSchema);
