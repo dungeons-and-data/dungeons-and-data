c725272835ad5570fb2e675d8d7ef1290b1b7ca6
Lewis Benson <tm.lewisbenson@gmail.com>
2022-12-23 09:50:57 -0500

    create User model and Schema

---
+++ server/models/User.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+
+const UserSchema = new Schema({
+  user_id: { type: String, required: true },
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  role: { type: String, required: true },
+
+});
+
+
+
+module.exports = mongoose.model('user', UserSchema);
