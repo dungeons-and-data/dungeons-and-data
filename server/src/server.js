efdb5be4724ab16ceb450b2de1ba25411c6ea0f2
Lewis Benson <tm.lewisbenson@gmail.com>
2022-12-23 10:13:39 -0500

    update express server for proof of life

--- server/src/server.js
+++ server/src/server.js
@@ -3,15 +3,18 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const PORT = process.env.PORT || 3002;
-const userRoute = require('./routes/user');
-const notFound = require('./error-handlers/404');
-const errorHandler = require('./error-handlers/500');
+const notFound = require('../error-handlers/404');
+const errorHandler = require('../error-handlers/500');
+
+
 
 app.use(express.json());
 app.use(cors());
 
-app.use(userRoute);
+
+
 app.get('/', (req, res, next) => {
+
   res.status(200).send('Proof of life');
 });
 
