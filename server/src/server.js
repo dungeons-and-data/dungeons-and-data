4558a7dfa12a288b5c9c9b40d4d2d0bf3c5a22ea
Lewis Benson <tm.lewisbenson@gmail.com>
2022-12-23 11:21:06 -0500

    add mongo to server

--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,22 @@
 require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const cors = require('cors');
 const PORT = process.env.PORT || 3002;
+const DATABASE_URL = process.env.DATABASE_URL
 const notFound = require('../error-handlers/404');
 const errorHandler = require('../error-handlers/500');
 
-
-
+mongoose.set('strictQuery', true);
+mongoose.connect(DATABASE_URL, { useNewUrlParser: true }, (error, connection) => {
+  if (error) {
+    console.error(error);
+  } else {
+    const port = connection.connections[0].port;
+    console.log(`Connected to MongoDB at ${'port'}:${port}`);
+  }
+});
 app.use(express.json());
 app.use(cors());
 
