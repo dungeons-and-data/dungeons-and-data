2e40250ba054ac6c979a21143687db611b1ddf75
Lewis Benson <tm.lewisbenson@gmail.com>
2022-12-24 15:09:41 -0500

    restructure data object in middleware

--- server/error-handlers/500.js
+++ server/error-handlers/500.js
@@ -2,8 +2,8 @@
 
 
 module.exports = function (err, req, res, next) {
-  const error = err.message ? err.message : err;
 
+  const error = err.message ? err.message : err;
   const errorObject = {
     status: 500,
     message: error,
