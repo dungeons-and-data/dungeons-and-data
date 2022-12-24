d5aba2ca06fb0fd6070d609f4ec72b149c31ae6c
Lewis Benson <tm.lewisbenson@gmail.com>
2022-12-24 15:10:21 -0500

    add test environment to mongodb

--- server/src/server.js
+++ server/src/server.js
@@ -13,7 +13,14 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const PORT = process.env.PORT || 3002;
 const SERVER_URL = process.env.SERVER_URL || 'localhost';
-const DATABASE_URL = process.env.DATABASE_URL;
+
+let DATABASE_URL;
+if (process.env.NODE_ENV === 'test') {
+  DATABASE_URL = process.env.DATABASE_URL_TEST;
+} else {
+  DATABASE_URL = process.env.DATABASE_URL_LIVE;
+}
+
 const notFound = require('../error-handlers/404');
 const errorHandler = require('../error-handlers/500');
 const userRouter = require('../routes/userRoutes');
@@ -21,14 +28,16 @@ const startIo = require('../socket-server');
 
 
 mongoose.set('strictQuery', true);
-mongoose.connect(DATABASE_URL, { useNewUrlParser: true, bufferCommands: false }, (error, connection) => {
-  if (error) {
+async function connectToMongoDB() {
+  try {
+    await console.log(`Connected to MongoDB at ${'DATABASE_URL'}`);
+    await mongoose.connect(DATABASE_URL, { useNewUrlParser: true, bufferCommands: false });
+  } catch (error) {
     console.error(error);
-  } else {
-    const port = connection.connections[0].port;
-    console.log(`Connected to MongoDB at ${'port'}:${port}`);
   }
-});
+}
+
+connectToMongoDB();
 
 startIo(io);
 
@@ -42,13 +51,13 @@ app.get('/', (req, res, next) => {
   res.status(200).send('Proof of life');
 });
 
-app.use('*', notFound);
+app.all('*', notFound);
 app.use(errorHandler);
 
 function start() {
   server.listen({
     host: SERVER_URL,
     port: PORT,
-  }, () => console.log(`listening on port: 3000`));
+  }, () => console.log(`listening on port: ${PORT}`));
 }
-module.exports = { start };
+module.exports = { start, server, connectToMongoDB };
