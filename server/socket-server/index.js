84df25e93f69b8b6aa1792f3462d5b1f372a6151
Lewis Benson <tm.lewisbenson@gmail.com>
2022-12-24 11:43:49 -0500

    build proof of life for socket server

---
+++ server/socket-server/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+
+const startIo = (io) => {
+  io.on('connection', socket => {
+    console.log('IO server connection');
+
+    socket.emit('proofOfLife', 'This is the proof of life');
+  });
+};
+
+module.exports = startIo;
--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,23 @@
 require('dotenv').config();
+
+
 const express = require('express');
-const mongoose = require('mongoose');
 const app = express();
+const http = require('http');
+const socketio = require('socket.io');
+const server = http.createServer(app);
+const io = socketio(server);
+
+const mongoose = require('mongoose');
+
 const cors = require('cors');
 const PORT = process.env.PORT || 3002;
+const SERVER_URL = process.env.SERVER_URL || 'localhost';
 const DATABASE_URL = process.env.DATABASE_URL;
 const notFound = require('../error-handlers/404');
 const errorHandler = require('../error-handlers/500');
 const userRouter = require('../routes/userRoutes');
+const startIo = require('../socket-server');
 
 
 mongoose.set('strictQuery', true);
@@ -19,6 +29,9 @@ mongoose.connect(DATABASE_URL, { useNewUrlParser: true, bufferCommands: false },
     console.log(`Connected to MongoDB at ${'port'}:${port}`);
   }
 });
+
+startIo(io);
+
 app.use(express.json());
 app.use(cors());
 
@@ -33,6 +46,9 @@ app.use('*', notFound);
 app.use(errorHandler);
 
 function start() {
-  app.listen(PORT, () => console.log(`listening on port: ${PORT}`));
+  server.listen({
+    host: SERVER_URL,
+    port: PORT,
+  }, () => console.log(`listening on port: 3000`));
 }
-module.exports = { start, app };
+module.exports = { start };
