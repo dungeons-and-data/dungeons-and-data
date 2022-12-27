2b8c79bead0e389a8a8e400f9fc72d4c0b63a289
Lewis Benson <tm.lewisbenson@gmail.com>
2022-12-27 11:21:38 -0500

    create selectedChar

--- clients/__tests__/axios/roleChange.test.js
+++ clients/__tests__/axios/roleChange.test.js
@@ -8,18 +8,14 @@ jest.mock('axios');
 
 describe('diffRole function', () => {
   test('returns correct data when called with valid input', async () => {
-    // Set up the mock to return a successful response
+
     axios.put.mockResolvedValue({ data: 'Success' });
 
-    // Set up the user object and change role value
     const user = { id: '123', token: '456' };
     const change = 'admin';
 
-    // Call the diffRole function and assert that the return value is correct
     const result = await diffRole(user, change);
     expect(result).toBe('Success');
-
-    // Assert that the mock was called with the correct arguments
     expect(axios.put).toHaveBeenCalledWith(
       `${url}users/${user.id}`,
       { username: user.username, password: user.password, role: change },
