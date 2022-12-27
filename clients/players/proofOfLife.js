ba7ad04ca6e388656a6dcdc12ef3ca8bc91b1e68
Lewis Benson <tm.lewisbenson@gmail.com>
2022-12-26 21:24:26 -0500

    update proof of life

--- clients/players/proofOfLife.js
+++ clients/players/proofOfLife.js
@@ -1,8 +1,8 @@
-// const { io } = require('socket.io-client');
+const { io } = require('socket.io-client');
 // const socket = io('http://localhost:3001/');
 const login = require('../UI/login')
 // socket.on('proofOfLife', proof => console.log(proof));
 
 
 
-login().then(data => console.log(data));
+// login().then(data => console.log(data));
